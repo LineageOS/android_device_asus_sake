d18e312ad7c75e86c7aa12084688b75ff85f969d
Dan Morrill <morrildl@google.com>
2010-03-05 16:16:19 -0800

    sake: Add standard feature flag indicating device supports WiFi.
    Change-Id: I6c30e5fed791db99b4d2382d74027902ab360675

--- device.mk
+++ device.mk
@@ -350,6 +350,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 PRODUCT_PACKAGES += \

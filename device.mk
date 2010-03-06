460a21a2ca5acb3379c044a3da53d0865f63845c
Dan Morrill <morrildl@google.com>
2010-03-05 16:16:19 -0800

    sake: Declare standard set of features for a GSM phone.
     * ZenFone 8 does not support CDMA.
    
    Change-Id: I5b10c52f9744b437f07ce2e4ee4f841c4fd22540

--- device.mk
+++ device.mk
@@ -301,6 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
 # USB

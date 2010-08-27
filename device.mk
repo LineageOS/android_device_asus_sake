99969c9a73eb85220bc8f928fc5f2ff49909bb32
Hung-ying Tyan <tyanh@google.com>
2010-08-27 18:08:19 +0800

    sake: Declare support for VOIP (Voice over IP).
     * As well as SIP (Session Initiation Protocol) which
       is a signaling protocol used for initiating, maintaining,
       and terminating real-time sessions that include voice,
       video and messaging applications.
    
    Change-Id: I87dd377b9bdc8f90f96f9d0e756c774f57df4095

--- device.mk
+++ device.mk
@@ -299,6 +299,10 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
+# Telephony
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
+
 # USB
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 

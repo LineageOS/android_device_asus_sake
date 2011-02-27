2b94d968e6f0cb2e8e6b5bb20f4d8b6d75ae620b
Mike Lockwood <lockwood@android.com>
2011-02-27 11:23:25 -0800

    sake: Add platform features for USB host and USB accessory support.
    Change-Id: I167de7c7e20a51201d7164ecba285da17d08ceda

--- device.mk
+++ device.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
 # USB
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 TARGET_HAS_DIAG_ROUTER := true
 TARGET_KERNEL_VERSION := 5.4

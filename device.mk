b8f0330ff8977d816cc75d110b41d414c6403246
Eino-Ville Talvala <etalvala@google.com>
2014-07-15 11:05:18 -0700

    sake: Declare that this is a full-capability Camera2 device.
     <feature name="android.hardware.camera.any" />
     <feature name="android.hardware.camera.level.full" />
     <feature name="android.hardware.camera.capability.manual_sensor" />
     <feature name="android.hardware.camera.capability.manual_post_processing" />
    
    Change-Id: I3ecaec9ac9519680842bb6de929c161cf6cd50dc

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \

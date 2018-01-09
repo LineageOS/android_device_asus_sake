3a3cb420919d27ff3517738c85c52545b04e3c04
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    sake: Build system telephony extension.
    Change-Id: If3aaf88a908b2e6f6a5c873820981a40390197f6

--- device.mk
+++ device.mk
@@ -319,7 +319,11 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \

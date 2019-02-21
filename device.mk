51fa1c599d94f28618b0531e3f581ae50360447f
Indranil Chakraborty <ichakrab@codeaurora.org>
2019-02-21 23:57:02 +0530

    sake: Build WFD AAC vendor library from source.
    Change-Id: I0e6f5bbfec5a0c741d15e87d8dc988125f18ff43

--- device.mk
+++ device.mk
@@ -343,3 +343,7 @@ $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.sake
+
+# WFD
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor

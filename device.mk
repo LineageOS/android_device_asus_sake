e54c513374e958ed34844cea1c169841083a0756
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    sake: Use the QTI Boot Control HAL.
    Change-Id: Ie5079fd4c40cb1f7414f2cba9f98d350f03047d8

--- device.mk
+++ device.mk
@@ -91,6 +91,9 @@ PRODUCT_PACKAGES += \
 
 # Boot Control
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
     bootctrl.lahaina \
     bootctrl.lahaina.recovery
 

4b8b41e0e932a4a8ce159f7f9f116247b8142221
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:12:37 -0700

    sake: init: Override camera service definition.
    Change-Id: If21f0c5ae1ea6d459d65ce7e49bedb215972e1a6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,10 +71,12 @@ TARGET_SCREEN_DENSITY := 440
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/asus_framework_matrix.xml \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/asus_manifest.xml \
     $(DEVICE_PATH)/manifest.xml
 
 DEVICE_MATRIX_FILE := \
--- device.mk
+++ device.mk
@@ -91,6 +91,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
+    init.asus.camera.rc \
     init.asus.recovery.rc \
     init.class_main.sh \
     init.qcom.class_core.sh \

f0e937d372f8e54b617c623378d1ad798a664a52
Edwin Wong <edwinwong@google.com>
2021-03-25 14:42:41 -0700

    sake: Update ClearKey plugin to DRM HAL v1.4.
    Change-Id: I6ee6e0ee82b1be59bfc2ea9fb6b78c3ffea3be60

--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)

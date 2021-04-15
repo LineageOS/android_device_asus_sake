f19348af6c3f6e7b60d74eab3d98bd8c7b4f05b3
Alexander Koskovich <zvnexus@outlook.com>
2021-04-14 17:53:26 -0700

    sake: Import QTI AIDL vibrator HAL from LA.UM.9.14.r1-17700-LAHAINA.0.
    Change-Id: Id6d4ae94388f56ebcee048875e0166864044cbf1

--- device.mk
+++ device.mk
@@ -336,3 +336,9 @@ $(call inherit-product, vendor/asus/sake/sake-vendor.mk)
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# Vibrator
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.sake

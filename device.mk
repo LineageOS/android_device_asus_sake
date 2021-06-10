cffcf8846d48e081dac6ce2577f5dc62420ff67c
LuK1337 <priv.luk@gmail.com>
2021-06-10 09:24:34 +0200

    sake: Build the QTI thermal HAL as seen in 30.11.51.83.
    Change-Id: I831e822aa9373ae4e0806739897603956df21340

--- device.mk
+++ device.mk
@@ -305,6 +305,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # USB
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 

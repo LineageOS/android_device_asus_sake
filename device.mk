e51455b2f4cfe0d372f2ca8c7f3817a08dc3ef18
Alexander Koskovich <zvnexus@outlook.com>
2021-07-14 20:14:29 -0700

    sake: Import cleaned QSSI overlays from LA.QSSI.11.0.r1-12400-qssi.0.
     * Removed overlays that either didn't exist or were
       already set to the value QCOM had upstream.
    
    Change-Id: I1b94e4b29c07f9b186fb10470d2fd083667dc7fc

--- device.mk
+++ device.mk
@@ -22,6 +22,13 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon \
+    FrameworksResCommon \
+    SystemUIResCommon \
+    TelephonyResCommon \
+    WifiResCommon
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 

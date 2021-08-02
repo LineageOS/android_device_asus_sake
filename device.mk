060eb6d14ba322f85ecaccb053fd4bcba4d0a785
Alexander Koskovich <zvnexus@outlook.com>
2021-08-01 21:40:55 -0700

    sake: Import the QTI AIDL lights HAL from LA.UM.9.14.r1-17700-LAHAINA.0.
    Change-Id: I86dccd1ae8b3b236f05dca79ad1f25f1e042fd48

--- device.mk
+++ device.mk
@@ -94,6 +94,9 @@ $(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
 
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd

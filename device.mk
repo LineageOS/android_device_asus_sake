98bafacc9373761c1095c8ddc68ebb64c3e22da4
Alexander Koskovich <zvnexus@outlook.com>
2021-08-06 01:19:54 -0700

    sake: Build the fingerprint service and declare support.
    Change-Id: Ic8eb351a4dd682efd2442c1486dc49a528887157

--- device.mk
+++ device.mk
@@ -49,6 +49,13 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot
 
+# Biometrics
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Bluetooth
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
 

b33da40b481b5ad18f77633f9264cd3c0bdf16a6
Alexander Koskovich <zvnexus@outlook.com>
2021-08-06 01:25:53 -0700

    sake: Build QTI camera interfaces for CamX.
    Change-Id: I23efb141c135c279f4bfb5c6f0b6c95b6c78a91d

--- device.mk
+++ device.mk
@@ -100,7 +100,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Display
 $(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.mk)

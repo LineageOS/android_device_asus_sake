37b96a6f939fe76298f0e30dc68229485a5dd9c3
Alexander Koskovich <zvnexus@outlook.com>
2021-08-09 03:59:43 -0700

    sake: Build remaining OSS dependencies of WFD.
    Change-Id: I7fd3d22b873d0c63f8b7c4a1027c336ae43fb8fd

--- device.mk
+++ device.mk
@@ -207,6 +207,7 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
+    libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
@@ -236,6 +237,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     kernel/asus/sm8350
+
+# Networking
+PRODUCT_PACKAGES += \
+    libnl
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *

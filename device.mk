e13d4feefab788b0ea0c02cb208fe4201ad45b66
Alexander Koskovich <zvnexus@outlook.com>
2021-08-21 18:56:18 -0700

    sake: Move USB Soong namespace to relevant repository.
    Change-Id: I1a731acdb56327a8f2e2951d92a5f99968fc0b3e

--- device.mk
+++ device.mk
@@ -356,7 +356,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 TARGET_HAS_DIAG_ROUTER := true
 TARGET_KERNEL_VERSION := 5.4
 

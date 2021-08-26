81c9ab460b2f548439a5aa560d2801374f70f425
Alexander Koskovich <akoskovich@pm.me>
2021-08-25 17:42:52 -0700

    sake: Switch to the OSS sound trigger HAL.
    Change-Id: If79dc91c13658656132c337bf0fc19649c3c060f

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.bcl.enabled=false \
     persist.vendor.audio.speaker.prot.enable=false \
@@ -46,9 +50,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product, hardware/qcom-caf/sm8350/audio/configs/lahaina/lahaina.mk)
 $(call inherit-product, vendor/qcom/opensource/audio/configs/qssi/qssi.mk)
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 PRODUCT_PACKAGES += \
     libbatterylistener \
@@ -62,7 +63,8 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libsndmonitor \
     libspkrprot \
-    libvolumelistener
+    libvolumelistener \
+    sound_trigger.primary.lahaina
 
 # Biometrics
 PRODUCT_COPY_FILES += \

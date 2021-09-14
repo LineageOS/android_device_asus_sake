8990837cc9e941c1bbbda8b054efa66a0b554be7
Alexander Koskovich <akoskovich@pm.me>
2021-09-13 19:55:38 -0700

    sake: Switch to stock camera & gallery apps
    Change-Id: Ia2915b9c6f3c64556aa792d6dadf729883af680a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -279,6 +279,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # WLAN
 BOARD_WLAN_DEVICE := qcwcn

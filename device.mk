52c649b428c38820390ce78122973b4620ab7061
Alexander Koskovich <akoskovich@pm.me>
2021-10-10 16:36:29 -0700

    sake: proprietary-files: Pull the WCNSS QCACLD configuration from stock.
     * We're checking out the QCACLD driver to the stock
       tag so this makes sense.
    
    Change-Id: I784b45c0c32b7253e4bf12dc7c2c6979dbde7a20

--- device.mk
+++ device.mk
@@ -376,7 +376,6 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/vendor_cmd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/vendor_cmd.xml \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \

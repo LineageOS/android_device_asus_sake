0f4ba345773fb5a306c82b211c2e96edf181226d
TheScarastic <warabhishek@gmail.com>
2021-10-30 08:30:42 +0200

    sake: fingerprint: Update HAL to v2.3 to support UDFPS.
    Change-Id: I5d33380a238ad33b3ca79c5ffe0b148cdfa57a8e

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2017-2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.sake"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.sake"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.sake"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.3-service.sake"
 
 #include <hardware/hw_auth_token.h>
 
+#include <android-base/file.h>
 #include <android-base/properties.h>
 #include <hardware/hardware.h>
 #include <hardware/fingerprint.h>
@@ -26,11 +27,21 @@
 #include <inttypes.h>
 #include <unistd.h>
 
+#define CMD_FINGER_DOWN 200001
+#define CMD_FINGER_UP 200003
+#define CMD_LIGHT_AREA_CLOSE 200000
+#define CMD_LIGHT_AREA_STABLE 200002
+#define CMD_PARTIAL_FINGER_DETECTED 200004
+
+#define GLOBAL_HBM_PATH "/proc/globalHbm"
+#define GLOBAL_HBM_ON "1"
+#define GLOBAL_HBM_OFF "0"
+
 namespace android {
 namespace hardware {
 namespace biometrics {
 namespace fingerprint {
-namespace V2_1 {
+namespace V2_3 {
 namespace implementation {
 
 // Supported fingerprint HAL version
@@ -47,6 +58,7 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     if (!mDevice) {
         ALOGE("Can't open HAL module");
     }
+    this->mGoodixFingerprintDaemon = IGoodixFingerprintDaemon::getService();
 }
 
 BiometricsFingerprint::~BiometricsFingerprint() {
@@ -62,6 +74,32 @@ BiometricsFingerprint::~BiometricsFingerprint() {
         return;
     }
     mDevice = nullptr;
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    this->mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_DOWN, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+    if (!android::base::WriteStringToFile(GLOBAL_HBM_ON, GLOBAL_HBM_PATH)) {
+        ALOGE("Failed to write to %s", GLOBAL_HBM_PATH);
+    }
+    this->mGoodixFingerprintDaemon->sendCommand(CMD_LIGHT_AREA_STABLE, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    if (!android::base::WriteStringToFile(GLOBAL_HBM_OFF, GLOBAL_HBM_PATH)) {
+        ALOGE("Failed to write to %s", GLOBAL_HBM_PATH);
+    }
+    this->mGoodixFingerprintDaemon->sendCommand(CMD_FINGER_UP, {},
+                                                [](int, const hidl_vec<signed char>&) {});
+
+    return Void();
 }
 
 Return<RequestStatus> BiometricsFingerprint::ErrorFilter(int32_t error) {
@@ -148,7 +186,7 @@ Return<uint64_t> BiometricsFingerprint::setNotify(
         const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
     std::lock_guard<std::mutex> lock(mClientCallbackMutex);
     mClientCallback = clientCallback;
-    // This is here because HAL 2.1 doesn't have a way to propagate a
+    // This is here because HAL 2.3 doesn't have a way to propagate a
     // unique token for its driver. Subsequent versions should send a unique
     // token for each call to setNotify(). This is fine as long as there's only
     // one fingerprint device on the platform.
@@ -251,7 +289,7 @@ fingerprint_device_t* BiometricsFingerprint::openHal() {
     }
 
     if (kVersion != device->version) {
-        // enforce version on new devices because of HIDL@2.1 translation layer
+        // enforce version on new devices because of HIDL@2.3 translation layer
         ALOGE("Wrong fp version. Expected %d, got %d", kVersion, device->version);
         return nullptr;
     }
@@ -335,6 +373,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
                         msg->data.authenticated.finger.gid,
                         token).isOk()) {
                     ALOGE("failed to invoke fingerprint onAuthenticated callback");
+                    getInstance()->onFingerUp();
                 }
             } else {
                 // Not a recognized fingerprint
@@ -362,7 +401,7 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
 }
 
 } // namespace implementation
-}  // namespace V2_1
+}  // namespace V2_3
 }  // namespace fingerprint
 }  // namespace biometrics
 }  // namespace hardware

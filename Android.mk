3370764b05b7c58020d7c53097a314e616e3f6b3
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 01:44:38 -0700

    sake: Move ueventd.qcom.rc to /vendor/etc.
     * This is the preferred location as of Android 12.
    
    Change-Id: I8c7d2230281b6d8da802bf10f88461289849acaf

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,8 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter sake,$(TARGET_DEVICE)),)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # A/B builds require us to create the mount points at compile time.
 # Just creating it for all cases since it does not hurt.

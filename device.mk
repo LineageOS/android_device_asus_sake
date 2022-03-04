f3059d14b0874d0b58c2b8a83da7b464f0327fb3
Alexander Koskovich <akoskovich@pm.me>
2022-03-04 08:46:58 -0700

    sake: Update to 31.1004.0404.107
    Change-Id: I9643df4fe665df13808537672674ce2855e58e26

--- device.mk
+++ device.mk
@@ -326,7 +326,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor
 
 # Security
-BOOT_SECURITY_PATCH := 2021-12-05
+BOOT_SECURITY_PATCH := 2022-01-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sensors

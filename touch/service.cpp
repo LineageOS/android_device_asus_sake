e0bd7390dc341177a3d7dded04f92ecf973b7b17
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-05 15:26:43 +0200

    sake: touch: Import dummy touchscreen gesture HAL
    Change-Id: I5f1b0c624be1a8a8a05ec8af8f2916cdc119c266

--- touch/service.cpp
+++ touch/service.cpp
@@ -9,17 +9,26 @@
 #include <hidl/HidlTransportSupport.h>
 
 #include "GloveMode.h"
+#include "TouchscreenGesture.h"
 
 using ::vendor::lineage::touch::V1_0::IGloveMode;
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
 using ::vendor::lineage::touch::V1_0::implementation::GloveMode;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
 
 int main() {
     android::sp<IGloveMode> gloveMode = new GloveMode();
+    android::sp<ITouchscreenGesture> touchscreenGesture = new TouchscreenGesture();
 
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     if (gloveMode->registerAsService() != android::OK) {
         LOG(ERROR) << "Cannot register touchscreen glove HAL service.";
+        return 1;
+    }
+
+    if (touchscreenGesture->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
         return 1;
     }
 

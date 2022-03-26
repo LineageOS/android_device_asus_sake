9b29a3fdf78cd9598d0f71f7d695e87484dc0292
Alexander Koskovich <akoskovich@pm.me>
2022-03-26 12:54:08 -0700

    sake: Drop system-side vndfwk libraries
     * The only libraries that appear to make use of these
       anymore are on /vendor
    
    Change-Id: I030075d200ebf5ab13d53c0b2fe85bc77e60544d

--- device.mk
+++ device.mk
@@ -304,9 +304,7 @@ TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 
 PRODUCT_PACKAGES += \
     libjson \
-    libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
-    libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
 # RIL

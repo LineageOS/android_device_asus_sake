6ce2374266362e88a3aa459f2be2ee25712865f4
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-21 13:12:20 -0400

    sake: Build vendor variant of hostapd interface lib
    Change-Id: I9a30715d3216db76fb145e41719a65dba9c5341a

--- device.mk
+++ device.mk
@@ -471,6 +471,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \

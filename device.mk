22320aedfe226f2d3f4c738742fdd6057486eb5f
Tommaso Fonda <fondatommaso2@gmail.com>
2023-01-18 16:39:37 +0100

    sake: Update to android.hardware.bluetooth.audio@2.1-impl
     * Adds new functionality required for Bluetooth LE.
    
    Change-Id: Ida66014e85f7f9a51d6b922bf5de7e072ccdfff4

--- device.mk
+++ device.mk
@@ -109,7 +109,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
     com.dsi.ant@1.0.vendor \

ba4858166d2eb664c8fff2cbc7a98d9a20423ced
Chenyang Zhong <zhongcy95@gmail.com>
2023-01-27 00:52:03 -0500

    sake: rootdir: avoid migrating foreground tasks to X1
    The Cortex-X1 core is quite power hungary. Let's only use it for
    top-app tasks. Avoid migrating foreground tasks to X1 to reduce
    unnecessary bumps of X1's frequency and save some battery.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I951c50236f27ef383c5b5f9a57651b5f46f956c5

--- init/init.kernel.post_boot-lahaina.sh
+++ init/init.kernel.post_boot-lahaina.sh
@@ -209,6 +209,7 @@ echo 325 > /proc/sys/kernel/walt_low_latency_task_threshold
 echo 0-3 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/kernel/sched_boost

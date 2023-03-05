b8f7b10d5c296c444f2b14af5dd11b971e74ff43
Davide Garberi <dade.garberi@gmail.com>
2023-03-05 20:57:59 +0100

    sake: health: Changes for sake
    Change-Id: Ib7a72ae116cffbf944333164685cc876e9a66689

--- health/ChargingControl.cpp
+++ health/ChargingControl.cpp
@@ -11,7 +11,7 @@
 #include <android/binder_status.h>
 #include <fstream>
 
-#define LOG_TAG "vendor.lineage.health-service.default"
+#define LOG_TAG "vendor.lineage.health-service.sake"
 
 namespace aidl {
 namespace vendor {
@@ -19,8 +19,7 @@ namespace lineage {
 namespace health {
 
 static const std::vector<ChargingEnabledNode> kChargingEnabledNodes = {
-    {"/sys/class/power_supply/battery/input_suspend", "0", "1"},
-    {"/sys/class/power_supply/battery/charging_enabled", "1", "0"},
+    {"/sys/class/asuslib/charging_suspend", "0", "1"},
 };
 
 static bool fileExists(const std::string& path) {

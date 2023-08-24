8cede2b325075afe0939d05cb2ad2c4c2ab52675
Michael Zh <mikooomichael@gmail.com>
2023-08-24 07:32:19 +0300

    sake: Add proprietary-firmware.txt
    Change-Id: I32266912d6c01d4e0b61772793f6656b959420e2

--- extract-files.py
+++ extract-files.py
@@ -51,6 +51,7 @@ blob_fixups: blob_fixups_user_type = {
 module = ExtractUtilsModule(
     'sake',
     'asus',
+    add_firmware_proprietary_file=True,
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,

8cc081d797891b340a9c82e0fdc2d67061c5c258
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 22:02:20 +0530

    sake: Disable UFFD GC
    Unable to determine UFFD GC flag for kernel version "5.4.268-qgki".
    You can fix this by explicitly setting PRODUCT_ENABLE_UFFD_GC to "true" or
    "false" based on the kernel version.
    1. Set PRODUCT_ENABLE_UFFD_GC to "true" if the kernel supports userfaultfd(2)
       and MREMAP_DONTUNMAP.
    2. Set PRODUCT_ENABLE_UFFD_GC to "false" otherwise.
    
    Change-Id: Ie77d9a86a7fe94e4cade1868ffea27194f724e03

--- device.mk
+++ device.mk
@@ -241,6 +241,9 @@ PRODUCT_PACKAGES += \
     ueventd.asus.rc \
     ueventd.qcom.rc
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml

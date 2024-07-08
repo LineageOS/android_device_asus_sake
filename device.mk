48d01dc45322eee48322ef81db7186fdb8f71bb5
Michael Bestas <mkbestas@lineageos.org>
2024-07-08 08:56:25 +0300

    sake: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I8227b06f0466b614f12fa9008402b92461ea3dd0

--- device.mk
+++ device.mk
@@ -263,11 +263,6 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 # Media
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
-
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
     libOmxAmrEnc \
--- extract-files.sh
+++ extract-files.sh
@@ -66,7 +66,7 @@ function blob_fixup() {
         ;;
 
     vendor/etc/media_codecs.xml|vendor/etc/media_codecs_lahaina.xml|vendor/etc/media_codecs_lahaina_vendor.xml|vendor/etc/media_codecs_yupik_v1.xml)
-        sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
+        sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d" "${2}"
         ;;
     esac
 }

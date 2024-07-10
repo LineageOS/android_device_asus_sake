8b35773f897b34d1202bbde3db07dfdedcf7aa6c
mikooomich <mikooomichael@gmail.com>
2024-07-09 20:53:10 -0400

    sake: init: Move thermal-engine.conf to device tree
    Change-Id: I6cf43ff7d19ea1da9e34da2bb3cda506d984b531

--- device.mk
+++ device.mk
@@ -434,6 +434,9 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 

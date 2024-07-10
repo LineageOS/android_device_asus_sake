15404b9dd9a3eb6ead11d39477f2afe688133817
mikooomich <mikooomichael@gmail.com>
2024-07-09 21:17:15 -0400

    sake: init: Do not set input boost
    Change-Id: Ic8c285750708fb33b1bf031f2afbee28d9b6f286

--- init/init.kernel.post_boot-lahaina.sh
+++ init/init.kernel.post_boot-lahaina.sh
@@ -224,14 +224,6 @@ fi
 echo 691200 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
 
-# configure input boost settings
-if [ $rev == "1.0" ]; then
-	echo "0:1382800" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-else
-	echo "0:1305600" > /sys/devices/system/cpu/cpu_boost/input_boost_freq
-fi
-echo 120 > /sys/devices/system/cpu/cpu_boost/input_boost_ms
-
 # configure governor settings for gold cluster
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/down_rate_limit_us

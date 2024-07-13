89e65390660a6a5d8a2ce17f40b683b90389a4bb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sake: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- device.mk
+++ device.mk
@@ -169,8 +169,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.lights-service.qti \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     lights.qcom \
@@ -180,7 +178,6 @@ PRODUCT_PACKAGES += \
     libsdmcore \
     libsdmutils \
     libtinyxml \
-    memtrack.default \
     vendor.display.config@1.0 \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0 \
@@ -192,7 +189,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # Fastboot
 PRODUCT_PACKAGES += \

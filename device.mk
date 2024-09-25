0b82ddc3a3dc82e7d81fe4888d1be14f1a11b3a6
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    sake: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -434,7 +434,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
 
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # Touch
 PRODUCT_PACKAGES += \

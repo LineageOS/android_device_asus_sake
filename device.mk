981eb0c3f79b83c1f058bc76fe351eceb12c0e1e
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sake: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -349,6 +349,11 @@ PRODUCT_PACKAGES += \
     Zenfone8Telephony
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Performance

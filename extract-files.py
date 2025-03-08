8fdc8df4b9044c1b47651bad43d2bb4d822e376c
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    sake: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- extract-files.py
+++ extract-files.py
@@ -40,7 +40,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
-        'libwpa_client',
     ): lib_fixup_remove,
 }
 blob_fixups: blob_fixups_user_type = {

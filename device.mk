66eb4a0be33afa959ed76a44757fc3523579c8a5
Mikooo <mikooomichael@gmail.com>
2025-03-20 12:53:57 -0400

    sake: overlay-lineage: Update Aperture configuration
    Change-Id: I76f6993b7008024237e15319011eb6c39d2d777b

--- device.mk
+++ device.mk
@@ -301,6 +301,9 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \

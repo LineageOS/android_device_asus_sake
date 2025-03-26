f11daad2405377ff140ebbaf51e8a6e87b3c9dfd
mikooomich <mikooomichael@gmail.com>
2025-03-26 16:47:01 -0400

    sake: Import NFC configs
    * From 33.0210.0210.332
    
    Change-Id: I65b078ffdc9b5132c062aa2b91c4d6532a3ed440

--- device.mk
+++ device.mk
@@ -276,6 +276,8 @@ PRODUCT_PACKAGES += \
 # NFC
 $(call inherit-product, vendor/nxp/nfc/nfc-vendor-product.mk)
 $(call inherit-product, vendor/nxp/secure_element/se-vendor-product.mk)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.nfc.ese.xml \

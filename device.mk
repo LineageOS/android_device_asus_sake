7a622c0792db19cfce3a71f5f88f462805c01847
Mikooo <mikooomichael@gmail.com>
2025-03-27 10:56:39 -0400

    sake: Remove NFC eSE
    * NFC_AR_00_7E800_15.0C.00_OpnSrc driver has no suport eSE driver for PN557
    
    Change-Id: I9af3cda49b779cdaf4ec35f1b731b9741e85e5cf

--- device.mk
+++ device.mk
@@ -276,7 +276,6 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     Tag
 
@@ -294,8 +293,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_eSE/android.hardware.se.omapi.ese.xml
-
-PRODUCT_USES_ESE := false
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
